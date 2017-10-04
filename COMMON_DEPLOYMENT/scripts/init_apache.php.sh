ac61b78b8df3409a59dfaa8e51e110c4016ddd1e
NicolasCharpentier <nicolas.charpentier78@gmail.com>
2017-10-04 13:00:03 +0200

    tadow

--- COMMON_DEPLOYMENT/scripts/init_apache.php.sh
+++ COMMON_DEPLOYMENT/scripts/init_apache.php.sh
@@ -3,4 +3,6 @@
 # sudo php /scripts/init_conf_apache.php apache=2.2 app=spitcheeSPA www=/var/www/ANGULAR_SPA
 
  #sudo php /scripts/init_conf_apache.php apache=2.2 app=spitcheeREST www=/var/www/SILEX_WEB_REST/web
- sudo php /scripts/init_conf_apache.php apache=2.4 app=spitcheeREST www=/var/www/SILEX_WEB_REST/web
+ #sudo php /scripts/init_conf_apache.php apache=2.4 app=spitcheeREST www=/var/www/SILEX_WEB_REST/web
+
+ sudo php init_conf_apache.php apache=2.2 app=spitcheeREST www=/var/www-realspitchee/SILEX_WEB_REST/web
